ce9714090c8782795e090480aa254c1e5c990cb3
Joshua Marie <joshstat126@gmail.com>
2026-09-15 12:20:30 +0800

    Improve codes for `Box Integration Test`

--- scripts/verify-box-use.R
+++ scripts/verify-box-use.R
@@ -1,19 +1,58 @@
-cat("about to call box::use()\n"); flush(stdout())
+say = function(...) {
+    cat(..., "\n", sep = "")
+    flush(stdout())
+}
+
+say("about to call box::use()")
+
 box::use(
+    # Custom `{purrr}` package
     fp = fpurrr,
     fpurrr/map,
+
+    # Custom "stats" module
+    stm = statmodule,
+    statmodule/random/draw
 )
 
-cat("box::use() returned\n"); flush(stdout())
+say("box::use() returned")
 
-cat("== fp$map$call(1:5, sqrt) ==\n"); flush(stdout())
+say("")
+say("== Using the attached entire package ==")
+
+say("")
+say("-- `{fpurrr}` under the alias `fp` --")
+
+say("fp$map$call(1:5, sqrt)")
 print(fp$map$call(1:5, sqrt))
 
-cat("\n== map$call(1:5, sqrt) (submodule imported directly) ==\n"); flush(stdout())
-print(map$call(1:5, sqrt))
-
-cat("\n== fp$map$call@dbl(1:5, sqrt) (typed dispatch) ==\n"); flush(stdout())
+say("")
+say("fp$map$call@dbl(1:5, sqrt) (typed dispatch)")
 print(fp$map$call@dbl(1:5, sqrt))
 
+say("")
+say("-- `{statmodule}` under the alias `stm` --")
+
+say("stm$regression$ols$linear_reg(mtcars, mpg ~ wt + disp)")
+print(stm$regression$ols$linear_reg(mtcars, mpg ~ wt + disp))
+
+say("")
+say("== Using submodules imported directly ==")
+
+say("")
+say("map$call(1:5, sqrt)")
+print(map$call(1:5, sqrt))
+
+say("")
+say("draw$normal(10, 5, 1)")
+print(draw$normal(10, 5, 1))
+
+say("")
+say("== Assertions ==")
+
 stopifnot(identical(fp$map$call@dbl(1:5, sqrt), sqrt(1:5)))
-cat("\n\nbox successfully imported the carrier-installed module.\n")
+stopifnot(identical(map$call(1:5, sqrt), fp$map$call(1:5, sqrt)))
+stopifnot(length(draw$normal(10, 5, 1)) == 10)
+
+say("")
+say("box successfully imported the carrier-installed modules.")
